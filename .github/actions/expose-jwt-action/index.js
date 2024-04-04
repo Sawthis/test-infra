231c34dac0e037155ee583c28081c8176831c788
Wojciech Sołtys <74361703+Sawthis@users.noreply.github.com>
2024-04-04 08:06:30 +0000

    Change input to env variable

--- .github/actions/expose-jwt-action/index.js
+++ .github/actions/expose-jwt-action/index.js
@@ -1,17 +1,13 @@
 const core = require('@actions/core');
-const github = require('@actions/github');
 
 async function run() {
 try {
   // Get aud and request token
-  const audience = core.getInput('audience');
-  console.log(`audience: ${audience}`);
+  const audience = process.env.NPUT_AUDIENCE;
+  console.log(`audience in javascript ${audience}`);
   const jwt = await core.getIDToken(audience);
   core.setOutput("jwt", jwt);
   core.exportVariable('JWT', jwt);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
 } catch (error) {
   core.setFailed(error.message);
 }
