bd99d4532148bb9877a2906258e70f780638dc40
Wojciech Sołtys <74361703+Sawthis@users.noreply.github.com>
2024-04-04 08:21:41 +0000

    Use Javascript Github action

--- .github/actions/expose-jwt-action/index.js
+++ .github/actions/expose-jwt-action/index.js
@@ -3,11 +3,11 @@ const core = require('@actions/core');
 async function run() {
 try {
   // Get aud and request token
-  const audience = process.env.NPUT_AUDIENCE;
+  const audience = core.getInput('audience');
   console.log(`audience in javascript ${audience}`);
   const jwt = await core.getIDToken(audience);
   core.setOutput("jwt", jwt);
-  core.exportVariable('JWT', jwt);
+  core.exportVariable('OUTPUT_JWT', jwt);
 } catch (error) {
   core.setFailed(error.message);
 }
